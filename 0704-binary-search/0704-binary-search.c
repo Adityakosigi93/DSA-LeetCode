16bc838734b879f764c1803e65476b3172ba0c78
Adityakosigi93 <85104331+Adityakosigi93@users.noreply.github.com>
2026-08-17 20:29:13 +0530

    Time: 0 ms (100%), Space: 10.1 MB (9.4%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,18 @@
+int search(int* nums, int numsSize, int target) {
+    int low=0,high=numsSize-1,mid=-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]>target){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return mid;
+    
+    
+}
