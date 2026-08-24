f947be4831283475f176d4d23e037db96bb0fc6e
Adityakosigi93 <85104331+Adityakosigi93@users.noreply.github.com>
2026-08-24 13:34:15 +0530

    Time: 0 ms (100%), Space: 8.8 MB (69.06%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,24 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    char *result=malloc((strlen(strs[0])+1)*sizeof(char));
+    int k=0,l=0,i,j;
+    if(strsSize == 1){
+        for(i=0; i<strlen(strs[0]); i++){
+            result[i]=strs[0][i];
+        }
+        result[i]='\0';
+        return result;
+    }
+    for(j=0; j<strlen(strs[0]); j++){
+
+        for(i=1; i<strsSize; i++){
+            if(strs[0][j] != strs[i][j]){
+                result[l]='\0';
+                return result;
+            }
+        }
+        result[l++]=strs[0][j];
+    }
+
+    result[l]='\0';
+    return result;
+}
