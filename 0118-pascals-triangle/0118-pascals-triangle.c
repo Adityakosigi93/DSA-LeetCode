8842e947f25eba3ecbb62ab67caf1dda4203c183
Adityakosigi93 <85104331+Adityakosigi93@users.noreply.github.com>
2026-09-25 21:57:52 +0530

    Time: 0 ms (100%), Space: 11.7 MB (33.55%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.c
@@ -0,0 +1,26 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    int **arr = malloc(numRows * sizeof(int *));
+    *returnColumnSizes = malloc(numRows * sizeof(int));
+    for(int i = 0; i < numRows; i++) {
+        arr[i] = malloc((i + 1) * sizeof(int));
+        (*returnColumnSizes)[i] = i + 1;
+        for(int j = 0, k = 0; j <= i; j++) {
+            if(j == 0 || j == i) {
+                arr[i][j] = 1;
+            }
+            else {
+                arr[i][j] = arr[i - 1][k] + arr[i - 1][k + 1];
+                k++;
+            }
+        }
+    }
+
+    *returnSize = numRows;
+
+    return arr;
+}
